b975901695c08df603dc074e8c5ab0c045564f87
Mark Mims <mark.mims@agiledynamics.com>
2011-09-27 05:13:47 -0600

    job configuration

---
+++ scripts/terasort.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SIZE=10000000
+NUM_MAPS=100
+NUM_REDUCES=100
+IN_DIR=in_dir
+OUT_DIR=out_dir
+
+hadoop /usr/lib/hadoop/hadoop-examples.jar teragen -Dmapred.map.tasks=${NUM_MAPS} ${SIZE} ${IN_DIR}
+
+sleep 10
+
+hadoop /usr/lib/hadoop/hadoop-examples.jar terasort -Dmapred.reduce.tasks=${NUM_REDUCES} ${IN_DIR} ${OUT_DIR}
+
